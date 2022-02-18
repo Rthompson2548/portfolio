4c31bad61e2a3ec63703c8c1007c3911b0b4efd4
reillythompson <80596387+Rthompson2548@users.noreply.github.com>
2022-02-18 11:24:41 -0800

    added ids and getElementById vars for each nav bar item

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+const { isFunctionTypeNode } = require("typescript");
+
 const page = document.querySelectorAll("page");
 const navBar = document.getElementById("nav-bar");
+
+const goToHomePage = document.getElementById("go-to-home");
 const home = document.getElementById("home");
+
+const goToAboutPage = document.getElementById("go-to-about");
 const about = document.getElementById("about");
+
+
+const goToToolkitPage = document.getElementById("go-to-toolkit");
 const toolkit = document.getElementById("toolkit");
+
+const goToProjectsPage = document.getElementById("go-to-projects");
 const projects = document.getElementById("projects");
+
+const goToContactPage = document.getElementById("go-to-contact")
 const contact = document.getElementById("contact");
+

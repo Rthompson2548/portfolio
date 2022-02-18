1731d482aa58a39dc7a70a6a0aa845b2331cabd8
reillythompson <80596387+Rthompson2548@users.noreply.github.com>
2022-02-18 11:14:51 -0800

    updated js file with getElementById & querySelectorAll for the page and page sections

---
+++ script.js
@@ -0,0 +1,7 @@
+const page = document.querySelectorAll("page");
+const navBar = document.getElementById("nav-bar");
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const toolkit = document.getElementById("toolkit");
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
